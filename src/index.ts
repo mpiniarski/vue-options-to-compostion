57d0780a266accbcd34b2d034dcf3659ff52bc13
marcin.piniarski <piniarski.m@gmail.com>
2024-06-06 15:31:14 +0200

    feature: handle mixin files parsing

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
 import path from 'path';
 import fs from 'fs/promises';
-import {readScriptTag, replaceScriptTag} from './vueFileManipulation';
+import { readScriptTag, replaceScriptTag } from './vueFileManipulation';
 import { transformComponent } from './transformComponent';
 
 // Main function to handle CLI input
 (async () => {
     const args = process.argv.slice(2);
     if (args.length === 0) {
-        console.error('Please provide the path to the Vue component file.');
+        console.error('Please provide the path to the component file.');
         process.exit(1);
     }
 
     const filePath = path.resolve(args[0]);
     const fileContent = await fs.readFile(filePath, 'utf-8');
-    try {
-        const scriptContent = await readScriptTag(fileContent);
-        const newScriptContent = transformComponent(scriptContent);
-        const newFileContent = replaceScriptTag(fileContent, newScriptContent);
+    const fileExtension = path.extname(filePath);
 
-        console.log(newFileContent);
+    try {
+        let scriptContent = fileContent;
+        let newScriptContent = '';
+
+        if (fileExtension === '.vue') {
+            scriptContent = await readScriptTag(fileContent);
+            newScriptContent = `<script setup>\n${transformComponent(scriptContent)}\n</script>`;
+            const newFileContent = replaceScriptTag(fileContent, newScriptContent);
+            console.log(newFileContent);
+        } else if (fileExtension === '.ts' || fileExtension === '.js') {
+            newScriptContent = transformComponent(scriptContent);
+            console.log(newScriptContent);
+        } else {
+            console.error('Unsupported file extension. Please provide a .vue, .ts, or .js file.');
+            process.exit(1);
+        }
     } catch (error) {
         console.error('Error transforming component:', error);
     }
