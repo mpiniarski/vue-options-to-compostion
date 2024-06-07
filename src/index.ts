bd86e0058fcafdcb2525805464ba258adb6a484b
marcin.piniarski <piniarski.m@gmail.com>
2024-06-07 15:57:23 +0200

    feature: index.ts handle destination file name

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import path from 'path';
 import fs from 'fs/promises';
-import { readScriptTag, replaceScriptTag } from './vueFileManipulation';
-import { transformComponent } from './transformComponent';
+import {readScriptTag, replaceScriptTag} from './vueFileManipulation';
+import {transformComponent} from './transformComponent';
 
 // Main function to handle CLI input
 (async () => {
     const args = process.argv.slice(2);
     if (args.length === 0) {
-        console.error('Please provide the path to the component file.');
+        console.error('Please provide the path to the source file.');
         process.exit(1);
     }
 
-    const filePath = path.resolve(args[0]);
-    const fileContent = await fs.readFile(filePath, 'utf-8');
-    const fileExtension = path.extname(filePath);
+    const sourceFilePath = path.resolve(args[0]);
+    const destinationFilePath = args[1]
+            ? path.resolve(args[1])
+            : path.join(path.dirname(sourceFilePath), `${path.basename(sourceFilePath, path.extname(sourceFilePath))}-composition${path.extname(sourceFilePath)}`);
+
+    const fileContent = await fs.readFile(sourceFilePath, 'utf-8');
+    const fileExtension = path.extname(sourceFilePath);
 
     try {
         let scriptContent = fileContent;
@@ -21,12 +25,16 @@ import { transformComponent } from './transformComponent';
 
         if (fileExtension === '.vue') {
             scriptContent = await readScriptTag(fileContent);
-            newScriptContent = `<script setup>\n${transformComponent(scriptContent)}\n</script>`;
+            newScriptContent = `<script setup>
+${transformComponent(scriptContent)}
+</script>`;
             const newFileContent = replaceScriptTag(fileContent, newScriptContent);
-            console.log(newFileContent);
+            await fs.writeFile(destinationFilePath, newFileContent, 'utf-8');
+            console.log(`Transformed file written to ${destinationFilePath}`);
         } else if (fileExtension === '.ts' || fileExtension === '.js') {
             newScriptContent = transformComponent(scriptContent);
-            console.log(newScriptContent);
+            await fs.writeFile(destinationFilePath, newScriptContent, 'utf-8');
+            console.log(`Transformed file written to ${destinationFilePath}`);
         } else {
             console.error('Unsupported file extension. Please provide a .vue, .ts, or .js file.');
             process.exit(1);
